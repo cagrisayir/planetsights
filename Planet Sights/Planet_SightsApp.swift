8e188318a54c724f82e9e83fea246837304f5802
Omer Cagri Sayir <omercagris@gmail.com>
2023-12-06 12:41:50 +0300

    Initial Commit

---
+++ Planet Sights/Planet_SightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Planet_SightsApp.swift
+//  Planet Sights
+//
+//  Created by Omer Cagri Sayir on 6.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Planet_SightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
